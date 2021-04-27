f41116217c49cacc2459802c6a0d50de20569a3b
adrian <adrian.gonzalez@pc-scol.fr>
2021-04-27 14:34:02 +0200

    initial commit

---
+++ src/main/java/com/example/openapiheadmappingissue/OpenapiHeadmappingIssueApplication.java
@@ -0,0 +1,13 @@
+package com.example.openapiheadmappingissue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenapiHeadmappingIssueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenapiHeadmappingIssueApplication.class, args);
+	}
+
+}
